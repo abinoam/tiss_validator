eac966f6a513f656a96399767d850d2c8147b682
Abinoam P. Marques Jr <abinoam@gmail.com>
2017-03-21 15:02:35 -0300

    Implement TissValidator.errors

--- lib/tiss_validator.rb
+++ lib/tiss_validator.rb
@@ -1,5 +1,16 @@
 require "tiss_validator/version"
+require 'nokogiri'
 
 module TissValidator
-  # Your code goes here...
+  TISS_SCHEMA_PATH =
+    File.expand_path "../tiss/030301/tissV3_03_01.xsd", __FILE__
+
+  def self.errors(file_name)
+    xsd = Nokogiri::XML::Schema(File.open(TISS_SCHEMA_PATH))
+    doc = Nokogiri::XML(File.read(file_name))
+    xsd.validate(doc).map do |error|
+      message = error.message.gsub /\{.*?\}/, ""
+      "Linha: #{error.line} - #{message}"
+    end
+  end
 end
